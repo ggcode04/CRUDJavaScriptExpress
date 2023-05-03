35ca4137963cddf97b5ce788d4349bd7fd15d1c9
Gabriel Granados Ureña <gabrig1404@gmail.com>
2023-05-03 16:52:33 -0600

    CRUD JS Express

---
+++ model/libro.js
@@ -0,0 +1,9 @@
+module.exports = {
+    obtener:function(conexion, funcion) {
+        conexion.query("SELECT * FROM libros", funcion)
+    },
+    insertar:function(conexion,datos, funcion) {
+        conexion.query("INSERT INTO libros ( nombre, imagen) VALUES (?,?)", [datos.nombre, datos.archivo], funcion);
+    }
+
+}
---
+++ routes/libros.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+const librosController = require('../controllers/librosController');
+
+/* GET home page. */
+router.get('/',librosController.index);
+router.get('/crear',librosController.crear);
+router.post('/', librosController.guardar);
+
+module.exports = router;
