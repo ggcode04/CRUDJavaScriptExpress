8cd56305b372aeb917555794e8b4ba4f3dee778a
Gabriel Granados Ureña <gabrig1404@gmail.com>
2023-05-09 23:43:28 -0600

    CRUD JS with Express

--- controllers/librosController.js
+++ controllers/librosController.js
@@ -1,5 +1,6 @@
 var conexion = require('../config/conexion.js');
 var libro = require('../model/libro.js');
+var borrar = require('fs');
 
 module.exports={
 
@@ -13,14 +14,73 @@ module.exports={
         });
 
     },
+    
+    // CREAR DATOS
     crear:function(req, res){
         res.render('libros/crear');
     },
+
+    // GUARDAR DATOS
     guardar:function(req, res){
         console.log(req.body);
+        console.log(req.file.filename);
 
-        libro.insertar(conexion, req.body, function(err){
+        libro.insertar(conexion, req.body, req.file, function(err){
             res.redirect('/libros')
         });
+    },
+
+    // ELIMINAR DATOS
+    eliminar:function(req, res){
+        console.log("Recepcion de datos");
+        console.log(req.params.id)
+
+        libro.retornarDatosID(conexion, req.params.id, function(err, registros){
+
+            var nombreImagen = "public/images/" + (registros[0].imagen);
+
+            if(borrar.existsSync(nombreImagen)){
+                borrar.unlinkSync(nombreImagen);
+            }
+
+            libro.borrar(conexion, req.params.id, function(err){
+                 res.redirect('/libros');
+            });
+        });
+    },
+
+    // Editar archivos
+    editar:function(req, res){
+
+        libro.retornarDatosID(conexion, req.params.id, function(err, registros){
+            console.log(registros[0]);
+            res.render('libros/editar', {libro:registros[0]});
+        });
+    },
+
+    // Actualizar archivos
+    actualizar:function name(req, res){
+        console.log(req.body.nombre);
+
+        if(req.file){
+            if(req.file.filename){
+                libro.retornarDatosID(conexion, req.body.id, function(err, registros){
+
+                    var nombreImagen = "public/images/" + (registros[0].imagen);
+        
+                    if(borrar.existsSync(nombreImagen)){
+                        borrar.unlinkSync(nombreImagen);
+                    }
+                    
+                    libro.actualizarArchivo(conexion, req.body, req.file, function(err){});
+                });   
+            }
+        }
+        
+        if(req.body.nombre){
+            libro.actualizar(conexion, req.body, function(err){});
+        }
+        
+        res.redirect('/libros/')
     }
 }
--- model/libro.js
+++ model/libro.js
@@ -1,9 +1,24 @@
 module.exports = {
     obtener:function(conexion, funcion) {
-        conexion.query("SELECT * FROM libros", funcion)
+        conexion.query("SELECT * FROM libros", funcion);
     },
-    insertar:function(conexion,datos, funcion) {
-        conexion.query("INSERT INTO libros ( nombre, imagen) VALUES (?,?)", [datos.nombre, datos.archivo], funcion);
-    }
 
+    insertar:function(conexion, datos, archivos, funcion) {
+        conexion.query("INSERT INTO libros ( nombre, imagen) VALUES (?,?)", [datos.nombre, archivos.filename], funcion);
+    },
+
+    retornarDatosID:function(conexion, id, funcion){
+        conexion.query("SELECT * FROM libros WHERE id=?", [id], funcion);
+    },
+
+    borrar:function(conexion, id, funcion){
+        conexion.query("DELETE FROM libros WHERE id=?", [id], funcion);
+    },
+
+    actualizar:function(conexion, datos, funcion) {
+        conexion.query("UPDATE libros SET nombre=? WHERE id=?", [datos.nombre, datos.id], funcion);
+    },
+    actualizarArchivo:function(conexion, datos, archivo, funcion) {
+        conexion.query("UPDATE libros SET imagen=? WHERE id=?", [archivo.filename, datos.id], funcion);
+    }
 }
